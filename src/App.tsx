8eba2e6735b1b6bac77839dddd184f6488aee01c
matheus <matheus@bolt360.com.br>
2024-10-09 12:47:34 -0300

    new branch

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { RepositoryList } from './components/RepositoryList'
+import './style/global.scss'
+
+export function App() {
+    return (
+        <RepositoryList />
+    )
+}
